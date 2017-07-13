1810be8a13095921b9ecec677c24c4d0ea1a2cb4
Mateus Lackome <a.mateuslol@gmail.com>
2017-07-12 21:57:07 -0300

    Image Octave

---
+++ IA.m
@@ -0,0 +1,15 @@
+x = dir ("*.jpg");
+arquivo=fopen('histograma.txt','a');
+for ii=1:length(x)
+img=imread(x(ii).name);
+flag=isgray(img);
+if(!flag)
+img=rgb2gray(img);
+endif
+hist=imhist(img);
+hist=hist';
+fprintf(arquivo,'%d,',hist);
+fprintf(arquivo,'%s','----categoria aqui----');
+fprintf(arquivo,'\n');
+endfor
+fclose(arquivo);
